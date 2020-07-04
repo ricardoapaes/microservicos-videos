34947ce56026e3a9a3f9a93142c886d63de19db3
Ricardo Paes <ricardo@likesistemas.com.br>
2020-07-04 08:19:54 -0300

    Definindo resurso Category

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'Api'], function() {
+    Route::resource('categories', 'CategoryController', [
+        'except' => ['create','edit']
+    ]);
 });
